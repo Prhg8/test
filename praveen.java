89c88b70055153785b482483bf01af4e6e19bb0b
Praveen <97902861+Prhg8@users.noreply.github.com>
2022-06-04 16:25:29 +0530

    Update praveen.java

--- praveen.java
+++ praveen.java
@@ -1,17 +1,4 @@
 Hello Surya!!!
   happy Birthday
   God bless you
-  Love you
-  Alll the best
-  Play ground
-how are you
-hello world
-hello srikanth
-helo
-hallaappanavar
-happy birthday my boy
-Euro Kids
-Sindhu
-Praveen
-maruthi
-Bangalore
+
